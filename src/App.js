649c7f7771cd7877ab2f19a73f71ce7d3323b7d7
Vinod Kumar <31367403+vinodadari@users.noreply.github.com>
2024-12-02 12:37:41 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+import './App.css';
+import index from './index.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header"> */}
+        <div style={{textAlign:"center"}}>
+          <img src={index} width="100%" height="50%" alt="" />
+          </div>
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      {/* </header> */}
+    </div>
+  );
+}
+
+export default App;
